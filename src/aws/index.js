dfb5f7d47bfc5032ed5ee82bd1eff889bd26c970
Dwain Robinson <robinson.dwain.developer@gmail.com>
2024-01-05 12:16:44 -0600

    Updating service: Updating s3Client configuration

--- src/aws/index.js
+++ src/aws/index.js
@@ -38,11 +38,19 @@ const {
   coverImageDistributionURI
 } = s3;
 
+console.log({
+  credentials: {
+    accessKeyId: s3AccessKeyId,
+    secretAccessKey: s3AecretAccessKey,
+    region
+  }
+});
+
 // Create S3 service object
 const s3Client = new S3Client({
   credentials: {
-    s3AccessKeyId,
-    s3AecretAccessKey,
+    accessKeyId: s3AccessKeyId,
+    secretAccessKey: s3AecretAccessKey,
     region
   }
 });
