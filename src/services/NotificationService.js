df78ed21d499558a7f01239f046f39883e317c4f
Dwain Robinson <robinson.dwain.developer@gmail.com>
2024-01-23 21:51:51 -0600

    Update to Notification service: return promise

--- src/services/NotificationService.js
+++ src/services/NotificationService.js
@@ -3,22 +3,24 @@
 import logger from '../logger';
 import { sgMailClient } from '../twilio';
 
-exports.sendEmail = async (toEmail, subject, content) => {
-  const params = {
-    to: toEmail,
-    from: noReplyEmail, // Use the email address or domain you verified above
-    subject,
-    html: content
-  };
+exports.sendEmail = (toEmail, subject, content) => {
+  return new Promise(async (resolve, reject) => {
+    const params = {
+      to: toEmail,
+      from: noReplyEmail, // Use the email address or domain you verified.
+      subject,
+      html: content
+    };
 
-  try {
-    const emailResponses = await sgMailClient.send(params);
-    const [response] = emailResponses;
-    if (response.statusCode < 300) {
-      resolve();
+    try {
+      const emailResponses = await sgMailClient.send(params);
+      const [response] = emailResponses;
+      if (response.statusCode < 300) {
+        resolve();
+      }
+    } catch (err) {
+      logger.error(`Error send email to user: ${toEmail}`, err);
+      reject(err);
     }
-  } catch (err) {
-    logger.error(`Error send email to user: ${toEmail}`, err);
-    reject(err);
-  }
+  });
 };
